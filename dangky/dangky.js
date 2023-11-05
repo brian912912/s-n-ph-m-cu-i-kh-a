1a29efc0c7487f453195dd85bcb958d4298ba90e
brian912912 <hovanngoc912@gmail.com>
2023-11-05 11:06:39 +0700

    merge

--- dangky/dangky.js
+++ dangky/dangky.js
@@ -25,8 +25,8 @@
 // })
 let accounts = [];
 const chuyentrang = document.getElementById("dangki")
-            chuyentrang.addEventListener( 'click', function(){
-                const username = document.getElementById('username').value;
+chuyentrang.addEventListener('click', function () {
+    const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     if (username && password) {
@@ -49,17 +49,19 @@ const chuyentrang = document.getElementById("dangki")
 function login() {
     const enteredUsername = document.getElementById('username1').value;
     const enteredPassword = document.getElementById('password1').value;
-      console.log(enteredUsername);
-      console.log(enteredPassword);
+    var accounts = [];
+    accounts = JSON.parse(localStorage.getItem("accounts"));
+    console.log(enteredUsername);
+    console.log(enteredPassword);
     if (enteredUsername && enteredPassword) {
-        const loggedInAccount = accounts.find(account => 
+        const loggedInAccount = accounts.find(account =>
             account.username === enteredUsername && account.password === enteredPassword
         );
 
         if (loggedInAccount) {
             alert('Đăng nhập thành công!');
             window.location.href = "../trang chủ/index.html";
-            } else {
+        } else {
             alert('Sai tên đăng nhập hoặc mật khẩu.');
         }
     } else {
@@ -67,7 +69,9 @@ function login() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+document.addEventListener('DOMContentLoaded', function () {
     if (localStorage.getItem('accounts')) {
         accounts = JSON.parse(localStorage.getItem('accounts'));
     }
